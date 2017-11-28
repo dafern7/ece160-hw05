7cbe1e78c846772e7def54670dd61effb6337e3d
Richard Fu <richard.fu123@gmail.com>
2017-11-28 14:30:58 -0500

    need to fix major stuff

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,45 @@
 #include "Student.h"
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+Student::Student(std::string f, std::string l,
+          int year, Major major) {
+			  _first_name = f;
+			  _last_name = l;
+			  _grad_year = year; 
+			  _major = major;
+		  }
+Student::~Student () {}
+
+
+void Student::addGrade (float g) {
+	_grades.push_back(g);
+}
+
+float Student::getGpa() {
+	float sum;
+	for (float &n : _grades) {
+		sum += n;	
+	}
+	float size = _grades.size();
+	return sum/size;
+}	
+/*std::string Student::majorString(Major m) {
+	std::string ret;
+	switch(m) {
+		case  "EE":
+		ret =  std::to_string(Major::EE);
+		break;
+		default: 
+		std::cout << "Invalid Major" << std::endl;
+	}
+	return ret;
+}
+*/
+void Student::printInfo(){
+	std::cout << _last_name << "," << 
+	_first_name << std::endl;
+	std::cout << _grad_year << std::endl;
+	std::cout << "GPA: " << getGpa() << std::endl;
+}
