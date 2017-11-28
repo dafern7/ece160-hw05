7ca515e208e6aacb17d260e4c285ddc236843fa1
Richard Fu <richard.fu123@gmail.com>
2017-11-28 16:38:45 -0500

    updated major

--- Student.cpp
+++ Student.cpp
@@ -1,5 +1,6 @@
 #include "Student.h"
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <stdio.h>
 
@@ -25,21 +26,41 @@ float Student::getGpa() {
 	float size = _grades.size();
 	return sum/size;
 }	
-/*std::string Student::majorString(Major m) {
+std::string Student::majorString(Major m) {
 	std::string ret;
 	switch(m) {
-		case  "EE":
-		ret =  std::to_string(Major::EE);
+		case Major::EE:
+		ret =  "EE";
+		break;
+		case Major::ME:
+		ret =  "ME";
+		break;
+		case Major::CE:
+		ret =  "CE";
+		break;
+		case Major::CHE:
+		ret =  "CHE";
+		break;
+		case Major::BSE:
+		ret =  "BSE";
+		break;
+		case Major::ART:
+		ret =  "ART";
+		break;
+		case Major::ARCH:
+		ret =  "ARCH";
 		break;
 		default: 
 		std::cout << "Invalid Major" << std::endl;
 	}
 	return ret;
 }
-*/
+
 void Student::printInfo(){
 	std::cout << _last_name << "," << 
 	_first_name << std::endl;
-	std::cout << _grad_year << std::endl;
+	std::cout << majorString(_major) << " " <<
+	_grad_year << std::endl;
+	std::cout << std::setprecision(2) << std::fixed;
 	std::cout << "GPA: " << getGpa() << std::endl;
 }
